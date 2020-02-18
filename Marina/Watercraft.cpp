0a8bf07762f622bc5d0467b9b16b2c859d350fcf
Joseph <joe.swales@googlemail.com>
2020-02-18 09:24:33 +0000

    Implemented ID and getters to Watercraft

--- Marina/Watercraft.cpp
+++ Marina/Watercraft.cpp
@@ -1,12 +1,36 @@
 #include "Watercraft.h"
 
-
+// Constants
+//Default Values
 
 Watercraft::Watercraft()
 {
+}
+
+Watercraft::Watercraft(int lengthValue, int berthValue)
+{
+	this->length = lengthValue;
+	this->berth = berthValue;
 }
 
 
 Watercraft::~Watercraft()
 {
 }
+
+//Getters
+unsigned Watercraft::getUniqueID()
+{
+	return uniqueID;
+}
+
+int Watercraft::getLength()
+{
+	return length;
+}
+
+int Watercraft::getBerth()
+{
+	return berth;
+}
+
--- Marina/Watercraft.h
+++ Marina/Watercraft.h
@@ -2,10 +2,33 @@
 class Watercraft
 {
 private:
+	unsigned uniqueID;//Unique Identifier
 
+	int length;
+	int berth;
 
 public:
 	Watercraft();
+	Watercraft(int, int);
 	~Watercraft();
+
+	//Getters and Setters
+	unsigned getUniqueID();
+	int getLength();
+	int getBerth();
+
+	//Equality Operators
+	bool operator==(const Watercraft &t);
+	bool operator!=(const Watercraft &t);
 };
 //Polymorphism is able to be used to Inherit to Specific types of Craft/Boat (narrow, sailing, or motor)
+
+//bool Watercraft::operator==()
+//{
+//
+//}
+//
+//bool Watercraft::operator!=()
+//{
+//
+//}
