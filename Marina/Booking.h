79ed1a6e651ab796ac8af67e606e7657860bdaf7
Joseph <joe.swales@googlemail.com>
2020-02-25 14:33:57 +0000

    Booking class implementation in header.

--- Marina/Booking.h
+++ Marina/Booking.h
@@ -1,8 +1,31 @@
 #pragma once
+
+#include "Watercraft.h"
+
+
+
+
 class Booking
 {
+private:
+
+	//Duration
+
+	int totalCost;//Cost
+
+	Watercraft* bookedCraft;//Watercraft pointer
+
 public:
 	Booking();
 	~Booking();
+
+	//Getters and Setters
+	int getTotalCost();
+	Watercraft* getBookedCraft();
+
+
+	//Methods
+	bool validLength();
+	bool validDraft();
 };
 
