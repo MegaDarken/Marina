ab0ff0a7d694f31f35f8a7b151bc5c2196a80527
Joseph <joe.swales@googlemail.com>
2020-03-02 17:22:47 +0000

    userInput Includes and view Records

--- Marina/userInput.cpp
+++ Marina/userInput.cpp
@@ -2,15 +2,22 @@
 
 void userInput::userRecordBooking()
 {
+	//
 
 }
 
 void userInput::userRecordDelete()
 {
+	//
 
 }
 
-void userInput::viewRecords()
+void userInput::viewRecords(std::vector<Booking> &bookingRecords)
 {
-
+	//for each of the vector's elements
+	for (size_t index = 0; index < bookingRecords.size(); index++)
+	{
+		//print record as string
+		std::cout << bookingRecords[index].getAsString() << std::endl;
+	}
 }
--- Marina/userInput.h
+++ Marina/userInput.h
@@ -1,4 +1,16 @@
 #pragma once
+
+#ifndef MARNIA_H
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <vector>
+
+#include "List.h"
+#include "Booking.h"
+#include "Watercraft.h"
+#endif // !MARNIA_H
+
 class userInput
 {
 private:
@@ -14,7 +26,7 @@ public:
 	static void userRecordDelete();
 
 	//3) display all records (and available marina space)
-	static void viewRecords();
+	static void viewRecords(std::vector<Booking> &bookingRecords);
 
 };
 
