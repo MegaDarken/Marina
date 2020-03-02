30f71ba070d025d4af3adca91d8ea97677af38dd
Joseph <joe.swales@googlemail.com>
2020-03-02 17:44:23 +0000

    Utilising memory optimised method

--- Marina/userInput.cpp
+++ Marina/userInput.cpp
@@ -14,10 +14,18 @@ void userInput::userRecordDelete()
 
 void userInput::viewRecords(std::vector<Booking> &bookingRecords)
 {
+	//Instantiate output char array
+	char* outputArray;
+
 	//for each of the vector's elements
 	for (size_t index = 0; index < bookingRecords.size(); index++)
 	{
+		//get record as string
+		bookingRecords[index].getAsString(outputArray);
+
 		//print record as string
-		std::cout << bookingRecords[index].getAsString() << std::endl;
+		std::cout << outputArray << std::endl;
 	}
+
+	//Remove the single char array from memory
 }
