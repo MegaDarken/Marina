5d2a2da34a3a73a7650b312c94da29bf6e0cc2a1
Joseph <joe.swales@googlemail.com>
2020-03-17 11:40:47 +0000

    Implemented List for Bookings

---
+++ Marina/BookingList.cpp
@@ -0,0 +1,59 @@
+
+#include "List.h"
+
+BookingList::BookingList()
+{
+	rootEntry = new BookingListEntry;//Start of the list's sequence
+
+	endEntry = rootEntry->nextEntry;
+	//currentEntry = rootEntry;
+}
+
+BookingList::~BookingList()
+{
+	rootEntry = nullptr;
+	delete rootEntry;
+	delete endEntry;
+}
+
+void BookingList::InsertEntry(Booking currentBooking)
+{
+	//Once at an Empty Entry
+	endEntry = new BookingListEntry;//Make Next Entry
+	endEntry->value = Booking(currentBooking);//Insert Value
+
+	//move end to next
+	endEntry = endEntry->nextEntry;
+}
+
+void BookingList::RemoveEntry(Booking currentBooking)
+{
+	BookingListEntry* currentEntry = rootEntry;//From the start
+
+	//for root node
+	if (currentEntry->value.getBookedCraft() == currentBooking.getBookedCraft())
+	{
+		//Move rootEntry to next
+		rootEntry = rootEntry->nextEntry;
+	}
+
+	{
+		//While the next Entry does not contain the object; Continue
+		while (currentEntry->nextEntry != nullptr)//currentEntry->nextEntry->value != currentCraft ||
+		{
+			currentEntry = currentEntry->nextEntry;//Move to next entry
+
+			//If the next entry is the one to be removed
+			if (currentEntry->nextEntry->value.getBookedCraft() == currentBooking.getBookedCraft())
+			{
+				BookingListEntry* chosenEntry = currentEntry->nextEntry;
+
+				//skip over the entry
+				currentEntry->nextEntry = chosenEntry->nextEntry;
+
+				delete chosenEntry;
+			}
+		}
+
+	}
+}
--- Marina/List.h
+++ Marina/List.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "Watercraft.h"
+#include "Booking.h"
 
 //This header file contains the structure of a Linked List of watercraft 
 struct WatercraftListEntry
@@ -21,4 +21,26 @@ public:
 
 	void InsertEntry(Watercraft);
 	void RemoveEntry(Watercraft);
+};
+
+//This header file contains the structure of a Linked List of bookings
+struct BookingListEntry
+{
+	Booking value;
+	BookingListEntry* nextEntry = NULL;
+};
+
+class BookingList
+{
+private:
+	BookingListEntry* rootEntry; //All other entries are based upon this. List Removed if changed.
+	BookingListEntry* endEntry;
+	//BookingListEntry* currentEntry; //Pointer for current node
+
+public:
+	BookingList();
+	~BookingList();
+
+	void InsertEntry(Booking);
+	void RemoveEntry(Booking);
 };
