769d481e706ca094cb0947fd8a560fe3e9f6f0ed
MegaDarken <joe.swales@googlemail.com>
2020-04-15 22:52:05 +0100

    Unit Test Project

---
+++ MarinaUnitTest/MarinaUnitTest.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+//Project includes
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace MarinaUnitTest
+{
+	TEST_CLASS(MarinaUnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
